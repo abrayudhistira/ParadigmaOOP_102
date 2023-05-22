3445d092d061fb4d134da929af5c7e26b7b3a3de
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2023-05-22 11:42:00 +0700

    membuat class orang

--- pewarisan/pewarisan/pewarisan.cpp
+++ pewarisan/pewarisan/pewarisan.cpp
@@ -2,7 +2,7 @@
 #include <string>
 using namespace std;
 
-class orang {
+class orang {									//membuat class orang
 public:
 	string nama;
 
@@ -18,3 +18,28 @@ public:
 		return a + b;
 	}
 };
+
+class pelajar :public orang {
+public:
+	string sekolah;
+
+	pelajar(string pNama, string pSekolah) :
+		orang(pNama),
+		sekolah(pSekolah) {
+		cout << "pelajar dibuat\n" << endl;
+	}
+	~pelajar() {
+		cout << "pelajar dihapus\n" << endl;
+	}
+	string perkenalan() {
+		return "Hallo, nama saya " + nama + " dari sekolah " + sekolah + "\n\n";
+	}
+};
+
+int main() {
+	pelajar siswa1("andi laksono", "SMAN 1 BANTUL");
+	cout << siswa1.perkenalan();
+	cout << "Hasil = " << siswa1.jumlah(90, 80) << endl;
+
+	return 0;
+}
